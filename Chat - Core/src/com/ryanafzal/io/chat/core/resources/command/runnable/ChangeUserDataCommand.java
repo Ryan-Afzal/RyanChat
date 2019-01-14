bec1e7723a086f6fd77a0e802270eb73c2b141c5
s-afzalr <s-afzalr@bsd405.org>
2019-01-14 10:44:07 -0800

    Work on the ChangeUserDataCommand

--- Chat - Core/src/com/ryanafzal/io/chat/core/resources/command/runnable/ChangeUserDataCommand.java
+++ Chat - Core/src/com/ryanafzal/io/chat/core/resources/command/runnable/ChangeUserDataCommand.java
@@ -1,11 +1,29 @@
 package com.ryanafzal.io.chat.core.resources.command.runnable;
 
+import java.util.HashMap;
+
 import com.ryanafzal.io.chat.core.resources.command.RunnableCommand;
+import com.ryanafzal.io.chat.core.resources.user.User;
 
+/**
+ * Updates data stored on the server. This can be permission levels, usernames, and more.
+ * @author s-afzalr
+ *
+ */
 public class ChangeUserDataCommand implements RunnableCommand {
-
-	public ChangeUserDataCommand() {
-		// TODO Auto-generated constructor stub
+	
+	public final long USERID;
+	public final long TARGETGROUPID;
+	public final HashMap<String, ?> PROPERTIES;
+	
+	public ChangeUserDataCommand(long userid, long targetGroupID, HashMap<String, ?> properties) {
+		this.USERID = userid;
+		this.TARGETGROUPID = targetGroupID;
+		this.PROPERTIES = properties;
+	}
+	
+	public ChangeUserDataCommand(long userid, HashMap<String, ?> properties) {
+		this(userid, -1, properties);
 	}
 
 	@Override
@@ -16,8 +34,7 @@ public class ChangeUserDataCommand implements RunnableCommand {
 
 	@Override
 	public Type getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return Type.SERVER;
 	}
 
 }
